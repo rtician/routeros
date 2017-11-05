c0514eb426523d16b59ff20852f487996924a799
Ramiro Tician <ramirotician@gmail.com>
2017-11-05 15:28:38 -0200

    Test encode and decode lengths.

--- tests/teste_api.py
+++ tests/teste_api.py
@@ -1,8 +1,9 @@
 import unittest
 from unittest.mock import Mock
+from collections import namedtuple
 from socket import SHUT_RDWR, error as SOCKET_ERROR
 
-from routeros.api import Socket
+from routeros.api import Socket, LengthUtils
 from routeros.exc import ConnectionError
 
 
@@ -46,3 +47,42 @@ class TestSocket(unittest.TestCase):
         self.transport.sock.recv.side_effect = SOCKET_ERROR
         with self.assertRaises(ConnectionError):
             self.transport.read(2)
+
+
+class TestLengthUtils(unittest.TestCase):
+    def setUp(self):
+        self.encoder = LengthUtils.encode_length
+        self.decoder = LengthUtils.decode_bytes
+
+        lengths = namedtuple('lengths', ('integer', 'encoded'))
+        self.valid_lengths = [
+            lengths(integer=0, encoded=b'\x00'),
+            lengths(integer=127, encoded=b'\x7f'),
+            lengths(integer=130, encoded=b'\x80\x82'),
+            lengths(integer=2097140, encoded=b'\xdf\xff\xf4'),
+            lengths(integer=268435440, encoded=b'\xef\xff\xff\xf0'),
+        ]
+
+    def test_encode_length(self):
+        for valid_length in self.valid_lengths:
+            result = self.encoder(valid_length.integer)
+            self.assertEqual(result, valid_length.encoded)
+
+    def test_encode_length_raises_if_lenghth_is_too_big(self):
+        # length must be < 268435456
+        invalid_length = 268435456
+
+        with self.assertRaises(ConnectionError):
+            self.encoder(invalid_length)
+
+    def test_decodeLength(self):
+        for valid_length in self.valid_lengths:
+            result = self.decoder(valid_length.encoded)
+            self.assertEqual(result, valid_length.integer)
+
+    def test_decodeLength_raises(self):
+        # len(length) must be < 5
+        invalid_bytes =b'\xff\xff\xff\xff\xff'
+
+        with self.assertRaises(ConnectionError):
+            self.decoder(invalid_bytes)
