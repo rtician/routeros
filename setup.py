182963f5e2b07340b3739e16135779bfd3f83f1e
Ramiro Tician <ramirotician@gmail.com>
2017-11-26 16:13:50 -0200

    Added setup configuration for pypi.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name='routeros',
+  packages=['routeros'],
+  version='1.0',
+  description='Implementation of Mikrotik API',
+  license='MIT',
+  author='Ramiro Tician',
+  author_email='ramirotician@gmail.com',
+  url='https://github.com/rtician/routeros',
+  download_url='https://github.com/rtician/routeros/archive/v0.1.tar.gz',
+  keywords=['mikrotik', 'routeros', 'api'],
+  classifiers=[
+      'Development Status :: 5 - Production/Stable',
+      'Intended Audience :: Developers',
+      'Operating System :: MacOS',
+      'Operating System :: Microsoft :: Windows',
+      'Operating System :: Unix',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Topic :: Software Development :: Libraries'
+  ],
+)
