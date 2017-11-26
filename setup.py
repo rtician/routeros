56026d002c92d0d59f2140663cf4d7ded6d8dc39
Ramiro Tician <ramirotician@gmail.com>
2017-11-26 16:34:58 -0200

    Fixed setup.y

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name='routeros',
   packages=['routeros'],
-  version='1.0',
+  version='0.1',
   description='Implementation of Mikrotik API',
   license='MIT',
   author='Ramiro Tician',
